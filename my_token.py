50b6eecfbb2cac2e5498407f6c16f65cce703021
ken <kennethpatrickfrancis@gmail.com>
2019-04-12 21:26:39 -0400

    update my_token.py with instructions

--- my_token.py
+++ my_token.py
@@ -1,3 +1,6 @@
-my_token = 'uHMTzVMXyTysPkWxNLPPPJQREhNRBSRabXimWMFN'                                                
-my_collection = 'data/kfrncs-collection-20190328-0840.csv'                                           
-my_wantlist = 'data/kfrncs-wantlist-20190328-0841.csv'  
+# PLEASE MAKE SURE THIS FILE IS DEFINITELY NOT ADDED TO ANY PULL REQUESTS
+# You can delete the access token later, but it's better safe than sorry.
+
+my_token = '<39AFKLSFN put your token here kfjJFSBASJBeq>'  # EDIT THIS to the access token from Discogs Developer Settings
+my_collection = 'data/my_collection_downloaded_from_discogs.csv' # EDIT THIS to the name of your csv, which you put in the data/ folder
+my_wantlist = 'data/my_wantlist_downloaded_from_discogs.csv'  # EDIT THIS, same as above
