979e1842920fcae5a8279c8eda2ec3a9151af157
Ken Francis <kennethpatrickfrancis@gmail.com>
2019-05-25 18:28:38 -0400

    reset index on df_discogs, bring in df_popsike

--- glue.py
+++ glue.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os 
+from joblib import load
 
 # get list of csv files that exist
 csv_list = os.listdir('data/pricefetch')
@@ -16,20 +17,24 @@ df_discogs = pd.concat(df_list)
 # make "date" column into datetime
 df_discogs['date'] = pd.to_datetime(df_discogs['date'])
 
-# drop extra columns -> index column, currency columns (it's all canadian)
+# drop extra columns -> erroneous index column, currency columns (it's all canadian)
 df_discogs = df_discogs.drop(columns=['Unnamed: 0'])
 cols = [c for c in df_discogs.columns if c.endswith('currency') != True]
 df_discogs = df_discogs[cols]
 
-# rename columns
+# rename columns and reset index
 df_discogs = df_discogs.rename(index=str, columns={
     'Fair (F).value': 'fair', 'Good (G).value': 'good', 'Good Plus (G+).value': 'good_plus',
     'Mint (M).value': 'mint', 'Near Mint (NM or M-).value': 'near_mint', 'Poor (P).value': 'poor',
     'Very Good (VG).value': 'vg', 'Very Good Plus (VG+).value': 'vg_plus', 'date': 'date',
     'release_id': 'release_id'})
+df_discogs = df_discogs.reset_index()
 
 # reorder columns
 df_discogs = df_discogs[['date', 'release_id', 'poor','fair', 'good', 'good_plus', 'vg', 'vg_plus', 'near_mint',  'mint']]
 
-df_popsike = pd.read_csv('data/popsike/release_list_may_24.joblib')
+# import popsike-scraped joblib file, put it into a df
+list_popsike = load('data/popsike/release_list_may_24.joblib')
+df_popsike = pd.DataFrame(list_popsike, columns=['release_id', 'year', 'title', 'artist', 'avg', 'min', 'max'])
+
 
