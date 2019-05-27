74eadf4695497a6e6c93f53c5f5317dfbcb543f2
Ken Francis <kennethpatrickfrancis@gmail.com>
2019-05-27 13:02:27 -0400

    add dropdown menu in flask

--- app.py
+++ app.py
@@ -2,9 +2,12 @@ import numpy as np
 from flask import Flask, request, render_template
 import pandas as pd
 import joblib
+import altair as alt
+import glue
 
 app = Flask(__name__)
 
+releases = ['test1', 'test2', 'test3', 'test4']
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -12,8 +15,10 @@ def index():
     if request.method == 'POST':
         stored_number += 1
 
+    # NOTE note: just slap a .serve() on the end of an altair chart to have it open up in another window
+
    # pass in the dictionary containing info here, eg. game=game from bot_paper_scissors        
-    return render_template('index.html', stored_number=stored_number) 
+    return render_template('index.html', stored_number=stored_number, releases=releases) 
 
 
 if __name__ == '__main__':
