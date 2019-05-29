5a9a7f2138f4e3eb9513e599a7b448582e0aefbe
Ken Francis <kennethpatrickfrancis@gmail.com>
2019-05-28 22:46:11 -0400

    add bad tooltip

--- app.py
+++ app.py
@@ -17,20 +17,16 @@ def index():
     if request.method == 'POST':
         chosen_release = int(request.form['release_dropdown'])
         df_current = df_popsike[df_popsike['release_id'] == chosen_release].copy()
-        base = alt.Chart(df_current.reset_index(), width=650).encode(x='year(year):O').properties(
+        base = alt.Chart(df_current.reset_index(), width=1150).encode(x='year(year):O').properties(
             title= df_current['title'].iloc[0] + ' - ' +  df_current['artist'].iloc[0]
         )
 
         alt.layer(
-            base.mark_line(color='red').encode(y='min:Q'),
-            base.mark_line(color='blue').encode(y='avg:Q'),
-            base.mark_line(color='green').encode(y='max:Q'),
+                base.mark_line(color='red').encode(y='min:Q', tooltip='min:Q'),
+                base.mark_line(color='blue').encode(y='avg:Q', tooltip='avg:Q'),
+                base.mark_line(color='green').encode(y='max:Q', tooltip='max:Q')
         ).interactive().serve()
 
-
-   # NOTE note: just slap a .serve() on the end of an altair chart to have it open up in another window
-
-   # pass in the dictionary containing info here, eg. game=game from bot_paper_scissors        
     return render_template('index.html', releases=releases, df_popsike=df_popsike, chosen_release=chosen_release) 
 
 
