9803eb928cdee8f73c614220d4546f6c3d7bf93f
Ken Francis <kennethpatrickfrancis@gmail.com>
2019-05-30 12:45:54 -0400

    make marketplace info print in server terminal

--- app.py
+++ app.py
@@ -7,6 +7,11 @@ import joblib
 import altair as alt
 import glue
 from glue import df_discogs, df_popsike
+import requests
+from bs4 import BeautifulSoup
+import re
+from market_scrape import market_scrape
+import pprint
 
 # my prophet.py, not fbprophet
 from prophet import log_transform, inverse_log_transform, prophesy
@@ -18,15 +23,18 @@ releases = list(df_popsike['release_id'].unique())
 @app.route('/', methods=['GET', 'POST'])
 def index():
     chosen_release = None
+    current_market = {}
     if request.method == 'POST':
         chosen_release = int(request.form['release_dropdown'])
         df_current = df_popsike[df_popsike['release_id'] == chosen_release].copy()
 
         ### prophet
         df_future = prophesy(df_current)
-        
 
-
+        current_market = market_scrape(chosen_release)
+        for i in range(len(current_market)):
+            print(current_market[i])
+            print()
         ### visualization
 
         # later desired behaviour (see chart.py)
@@ -52,7 +60,8 @@ def index():
         # you'll need this for prophet viz:
         # mark_line(strokeDash=[1,1])
 
-    return render_template('index.html', releases=releases, df_popsike=df_popsike, chosen_release=chosen_release) 
+    return render_template('index.html', releases=releases, df_popsike=df_popsike, chosen_release=chosen_release,
+            current_market=current_market) 
 
 
 if __name__ == '__main__':
